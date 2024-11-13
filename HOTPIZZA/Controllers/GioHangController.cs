00d138c2aae5e5d24c9ec63ad4a7a36c862157e6
thanhtrucne <2224802010095@student.tdmu.edu.vn>
2024-11-13 17:31:37 +0700

    d

--- HOTPIZZA/Controllers/GioHangController.cs
+++ HOTPIZZA/Controllers/GioHangController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HOTPIZZA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +9,12 @@ namespace HOTPIZZA.Controllers
 {
     public class GioHangController : Controller
     {
+        HOTPIZZAEntities1 db = new HOTPIZZAEntities1();
         // GET: GioHang
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult DatHang()
-        {
-            return View();
-        }
+        
     }
 }
