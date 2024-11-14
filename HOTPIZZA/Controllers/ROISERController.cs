c7f2347e282d1552260621825c8955a340123a1d
DieuLinh <dieu.diulinh@gmail.com>
2024-11-14 23:49:14 +0700

    UGU

---
+++ HOTPIZZA/Controllers/ROISERController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOTPIZZA.Controllers
+{
+    public class ROISERController : Controller
+    {
+        // GET: ROISER
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
