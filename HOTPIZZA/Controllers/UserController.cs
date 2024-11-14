d4aa4b388573fc94bddb2ac1851d1ecb274a2501
thanhtrucne <2224802010095@student.tdmu.edu.vn>
2024-11-14 10:33:15 +0700

    dang nhap

--- HOTPIZZA/Controllers/UserController.cs
+++ HOTPIZZA/Controllers/UserController.cs
@@ -8,14 +8,86 @@ namespace HOTPIZZA.Controllers
 {
     public class UserController : Controller
     {
+        NguoiDung _nd;
         HOTPIZZAEntities1 db = new HOTPIZZAEntities1();
-        // dang ky
-        public ActionResult DangKy()
+        public ActionResult Index()
         {
             return View();
         }
+        // dang ky
+        public ActionResult DangKy()
+        {
+            _nd = (NguoiDung)Session["user"];
+            if (_nd != null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangKy(NguoiDung khh)
+        {
+            _nd = (NguoiDung)Session["user"];
+            if (_nd != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(khh);
+            }
+            _nd = new NguoiDung();
+            _nd.HovaTen = khh.HovaTen;
+            _nd.NgaySinh = khh.NgaySinh;
+            _nd.Phone = khh.Phone;
+            _nd.Email = khh.Email;
+            _nd.TenDN = khh.TenDN;
+            _nd.MatKhau = khh.MatKhau;
 
-        
+            db.NguoiDungs.Add(_nd);
+            db.SaveChanges();
+            Session.Add("user", _nd);
+            return RedirectToAction("Index", "Home");
+        }
+
         //dang nhap
+        public ActionResult DangNhap()
+        {
+            _nd = (NguoiDung)Session["user"];
+            if (_nd != null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangNhap(Login lg)
+        {
+            _nd = (NguoiDung)Session["user"];
+            if (_nd != null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if (ModelState.IsValid)
+            {
+                _nd = db.NguoiDungs.SingleOrDefault(n => n.TenDN == lg.TenDN && n.MatKhau == lg.MatKhau);
+                if (_nd != null)
+                {
+                    Session.Add("user", _nd);
+                    return RedirectToAction("Index", "Home");
+                }
+                ViewBag.Fail = "Đăng nhập thất bại";
+                return View("DangNhap");
+            }
+            return View(lg);
+        }
+        public ActionResult DangXuat()
+        {
+            Session["user"] = null;
+            return RedirectToAction("Index", "Home");
+
+        }
     }
 }
