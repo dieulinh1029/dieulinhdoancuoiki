b67b29fbe331a1db3038182468b93f7c4d447b14
thanhtrucne <2224802010095@student.tdmu.edu.vn>
2024-11-15 22:45:11 +0700

    ưdsfgedwged

---
+++ HOTPIZZA/Controllers/TuyenDungController.cs
@@ -0,0 +1,29 @@
+﻿using HOTPIZZA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOTPIZZA.Controllers
+{
+    public class TuyenDungController : Controller
+    {
+        private HOTPIZZAEntities1 db= new HOTPIZZAEntities1();
+        // GET: TuyenDung
+        public ActionResult Index()
+        {
+            var c = from s in db.TuyenDungs
+                    
+                    select s;
+            return PartialView(c);
+        } 
+        public ActionResult ycTuyenDung(int id)
+        {
+            var m = from s in db.TuyenDungs
+                    where s.IdBaiTuyenDung == id
+                    select s;
+            return PartialView(m);
+        }
+    }
+}
