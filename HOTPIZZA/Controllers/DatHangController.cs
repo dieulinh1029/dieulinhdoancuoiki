bfe2e7dcfb2d15b435b003d4de2d938e3e80ba96
thanhtrucne <2224802010095@student.tdmu.edu.vn>
2024-11-15 14:43:38 +0700

    a

--- HOTPIZZA/Controllers/DatHangController.cs
+++ HOTPIZZA/Controllers/DatHangController.cs
@@ -4,17 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Linq.Expressions;
+using System.Data.Entity.Infrastructure;
+using System.Reflection;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Net;
 namespace HOTPIZZA.Controllers
 {
     public class DatHangController : Controller
     {
         private HOTPIZZAEntities1 db = new HOTPIZZAEntities1();
-        private List<MonAn> monAns = new List<MonAn>();
-        // GET: DatHang
+        private List<MonAn> ShowMon(int count)
+        {
+            return db.MonAns.OrderByDescending(a=>a.TenMon).Take(count).ToList();
+        }
         public ActionResult Index()
         {
-            return View();
+            var gg = ShowMon(8);
+            if (gg != null)
+            {
+                return View(gg);    
+            }
+            else
+            { 
+                return View();
+            }
+           
+        }
+        public ActionResult Chitiet(string id)
+        {
+            var mon = from s in db.MonAns
+                      where s.IdMon == id
+                      select s;
+            return View(mon.Single());
         }
     }
 }
